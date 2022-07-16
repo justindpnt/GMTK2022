ddc2dd15b7d2c154f6ade673cfe162904f0069e9
Justin DuPont <31671324+justindpnt@users.noreply.github.com>
2022-07-15 20:53:38 -0500

    Fully functional level moving based movement

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -6,14 +6,16 @@ public class Movement : MonoBehaviour
 {
     private Vector2 startingMousePosition;
     public float divisor = 100f;
-    public float speed = 1f;
     Quaternion tilt;
-    
+    public float maxXTiltAngle = 30;
+    public float maxYTiltAngle = 30;
+    public GameObject ball;
+    public float speed = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        startingMousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        startingMousePosition = new Vector2(Screen.width/2, Screen.height/2);
     }
 
     // Update is called once per frame
@@ -25,11 +27,29 @@ public class Movement : MonoBehaviour
         //Debug.Log(directionVector);
 
         float xMovement = directionVector.x / divisor;
-        float yMovement = directionVector.y / divisor * 2;
+        float yMovement = - (directionVector.y / divisor * 3);
+
+        if (xMovement > maxXTiltAngle)
+        {
+            xMovement = maxXTiltAngle;
+        }
+        if(xMovement < -maxXTiltAngle)
+        {
+            xMovement = -maxXTiltAngle;
+        }
+        if(yMovement > maxYTiltAngle)
+        {
+            yMovement = maxYTiltAngle;
+        }
+        if(xMovement < -maxYTiltAngle)
+        {
+            yMovement = -maxYTiltAngle;
+        }
+
 
         Debug.Log("x: " + xMovement + "y: " +  yMovement);
 
-        tilt = Quaternion.Euler(-yMovement, 0, xMovement);
+        tilt = Quaternion.Euler(yMovement, 0, xMovement);
     }
 
     private void FixedUpdate()
